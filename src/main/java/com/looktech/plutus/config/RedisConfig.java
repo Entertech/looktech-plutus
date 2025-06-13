cc56b4fc4be6e110dc0ffbe034e1bbac813f684e
wuhuyun <wuhuyun@126.com>
2025-06-13 16:22:38 +0800

    chore: Update Redis configuration to support both cluster and standalone modes. Enhance RedisConfig for dynamic connection setup based on environment properties. Improve logging in GlobalExceptionHandler for better error traceability.

--- src/main/java/com/looktech/plutus/config/RedisConfig.java
+++ src/main/java/com/looktech/plutus/config/RedisConfig.java
@@ -1,24 +1,85 @@
 package com.looktech.plutus.config;
 
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.core.env.Environment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
+import java.util.Arrays;
 
+@Slf4j
 @Configuration
 @EnableCaching
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        boolean isClusterMode = Boolean.parseBoolean(environment.getProperty("spring.redis.cluster.enabled", "false"));
+        
+        if (isClusterMode) {
+            return createClusterConnectionFactory();
+        } else {
+            return createStandaloneConnectionFactory();
+        }
+    }
+
+    private RedisConnectionFactory createClusterConnectionFactory() {
+        String clusterNodes = environment.getProperty("spring.redis.cluster.nodes");
+        String password = environment.getProperty("spring.redis.password");
+        
+        log.info("Initializing Redis Cluster Connection Factory");
+        log.info("Cluster Nodes: {}", clusterNodes);
+        
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration(Arrays.asList(clusterNodes.split(",")));
+        if (password != null && !password.isEmpty()) {
+            clusterConfig.setPassword(password);
+        }
+        
+        return new LettuceConnectionFactory(clusterConfig);
+    }
+
+    private RedisConnectionFactory createStandaloneConnectionFactory() {
+        String host = environment.getProperty("spring.redis.host", "localhost");
+        int port = Integer.parseInt(environment.getProperty("spring.redis.port", "6379"));
+        String password = environment.getProperty("spring.redis.password");
+        int database = Integer.parseInt(environment.getProperty("spring.redis.database", "0"));
+        
+        log.info("Initializing Redis Standalone Connection Factory");
+        log.info("Host: {}, Port: {}, Database: {}", host, port, database);
+        
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setDatabase(database);
+        if (password != null && !password.isEmpty()) {
+            config.setPassword(password);
+        }
+        
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        log.info("Initializing RedisTemplate with connection factory");
+        
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
@@ -26,11 +87,14 @@ public class RedisConfig {
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.afterPropertiesSet();
+        
         return template;
     }
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        log.info("Initializing RedisCacheManager with connection factory");
+        
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
--- src/main/java/com/looktech/plutus/exception/GlobalExceptionHandler.java
+++ src/main/java/com/looktech/plutus/exception/GlobalExceptionHandler.java
@@ -5,14 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CreditException.class)
     public ResponseEntity<Map<String, Object>> handleCreditException(CreditException ex) {
+        log.error("CreditException occurred: {}", ex.getMessage());
         Map<String, Object> response = new HashMap<>();
         response.put("code", ex.getCode());
         response.put("message", ex.getMessage());
@@ -20,7 +24,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
+public ResponseEntity<Map<String, Object>> handleException(Exception ex) {        
+        log.error("Unexpected error occurred", ex);
         Map<String, Object> response = new HashMap<>();
         response.put("code", "INTERNAL_ERROR");
         response.put("message", "An internal error occurred");
